d4e878a88b639116763fb8b81972d5fab834cb05
jchooch <joechoochoy@gmail.com>
2021-11-18 12:27:30 -0500

    finished transliteration, but still buggy

--- python/pretectum_RNN_master.py
+++ python/pretectum_RNN_master.py
@@ -20,22 +20,24 @@ visualize = True
 with_stimuli = True
 freeze_weights = False # wtf is the freezing about? figure out
 nFree = 5 # epochs to freeze learning before final epoch. what does this mean? figure out
+nFreePre = 0  # how many epochs from first one that learning is frozen
+num_of_inputs = 8
 
 ## Load Data
 znn_acts = pd.read_csv('znn_acts.csv', header=None) # create pandas dataframe of recorded znn calcium activities
 if with_stimuli == True:
-    stimcourse = pd.read_csv('stimcourse.csv', header=None) # create pandas dataframe of stimuli metadata (which stimuli when). Row order: LR,LU,LL,LD,RR,RU,RL,RD 
+    stim_course = pd.read_csv('stimcourse.csv', header=None) # create pandas dataframe of stimuli metadata (which stimuli when). Row order: LR,LU,LL,LD,RR,RU,RL,RD 
 
 ## Model Hyperparameters (exogenous to data)
-learn = 1 # what does this do?
-nRunTot = 1500 # number of steps (what is a step? an epoch?)
+learn = True # what does this do?
+nRunTot = 1500 # number of steps (what is a step? an epoch?) [maybe this should be called number of runs]
 amp_rgc = 0.025 # scale/amplitude of RGC inputs; Andalman et al. recommended 10x larger than noise amplitude
 # something about a control parameter here...
-g = 1.25;   # Peak synaptic conductance constant; Andalman used 1.2-1.3. Elsewhere 1.0-1.5 recommended. (Where?)
-dtData = 0.5; # Frame rate for experimental biological data in seconds
-dtModel = 0.25; # Integration step rate in seconds. Andalman et al. used 0.25
-tau = 2.5; # Neuron membrane time constants; Andalman et al. used 2.5
-P0 = 0.1; # What is this? Whit says "free parameter to learning rule for J". Sussillo & Abbott advise 0.01-1
+g = 1.25   # Peak synaptic conductance constant; Andalman used 1.2-1.3. Elsewhere 1.0-1.5 recommended. (Where?)
+dtData = 0.5 # Frame rate for experimental biological data in seconds
+dtModel = 0.25 # Integration step rate in seconds. Andalman et al. used 0.25
+tau = 2.5 # Neuron membrane time constants; Andalman et al. used 2.5
+P0 = 0.1 # What is this? Whit says "free parameter to learning rule for J". Sussillo & Abbott advise 0.01-1
 
 ## Model Parameters (endogenous to data)
 N = znn_acts.shape[0] # Number of znn neurons (number of rows in znn_acts); also, set N to be a double or floating point or whatever (why? I don't know)
@@ -62,8 +64,9 @@ def fit(nFreePre=0):
 lastchi2s = np.zeros(len(real_times))
 iModelSample = np.zeros(len(real_times))
 
-for i in np.range(len(real_times)):
-    tModelSample, iModelSample[i] = np.min(np.abs(real_times[i] - model_times))
+for i in range(len(real_times)):
+    tModelSample = np.min(np.abs(model_times - real_times[i]))     # WHAT IS THIS DOING?
+    iModelSample[i] = np.min(np.abs(model_times - real_times[i]))  # WHAT IS THIS DOING?
 '''WTF is this...
 for i=1:length(real_times)
     [tModelSample, iModelSample(i)] = min(abs(real_times(i)-t));
@@ -74,154 +77,115 @@ stdevData = np.std(znn_acts.to_numpy().flatten()) # why is this computed all the
 
 # White Noise
 WN_amp = 0.0025 # where does this value come from? ("amplitude no more than 1% of baseline according to Andalman")
-WN_input = WN_amp * np.random.randn(N, len(model_times))
+WN_input = WN_amp * np.random.randn(N, len(model_times)) # used to be inputN
 
 # Initialize 
-J0 = g * np.random.randn(N, N) / np.sqrt(N) # initialize weight matrix with Gaussian samples scaled by peak conductance constant
-R = np.nan(N, len(t)); # rnn (unit) activities
-AR = np.nan(N+8, length(t)); #augmented activity for training input wts
-JR = np.zeros(N, 1); #product of weights and nonlinear activity
-J = J0
+#J0 = g * np.random.randn(N, N) / np.sqrt(N)
+#J = J0
+J = g * np.random.randn(N, N) / np.sqrt(N) # initialize weight matrix with Gaussian samples scaled by peak conductance constant
 
-%inputs - e.g. LU means Left eye Up stim
-[inputLR,inputLU,inputLL,inputLD,inputRR,inputRU,inputRL,inputRD] = deal(zeros(N,length(t)));
+#################################### UP TO HERE ####################################
 
-%external input weights
-[wLR0,wLU0,wLL0,wLD0,wRR0,wRU0,wRL0,wRD0] = deal(randn(N,1)/sqrt(N));
-wLR = wLR0;wLU = wLU0;wLL = wLL0;wLD = wLD0;wRR = wRR0;wRU = wRU0;wRL = wRL0;wRD = wRD0;
+R = np.empty((N, len(model_times))) # RNN unit activities *** AM I RIGHT IN USING MODEL TIMES HERE? ***
+AR = np.empty((N + num_of_inputs, len(model_times))) # augmented activity for training input wts *** WHAT IS THIS AND WHY +8? ***
+JR = np.empty((N, 1)) # product of weights and unit activity
 
-if (learn)
-        PJ = P0 * eye(N+8, N+8);
-    end
-    
-    [data_units, data_steps] = size(Adata);
-    fprintf('data_steps: %d\n', data_steps);
-    fprintf('dt: %f\n', dtModel);
-    fprintf('length(t): %d\n', length(t));
-    assert(data_steps >= length(t) * dtModel);
+# Initialize external input weights
+W_input = np.random.randn(N, num_of_inputs) / np.sqrt(N)
 
-chi2 = zeros(1, nRunTot);
-pVars = zeros(1, nRunTot);
-for nRun = 1:nRunTot %Number of epochs
-    H = Adata(:, 1); %Initialize activities of all neurons with real values at first time point
-    R(:, 1) = tanh(H); %nonlinearly transformed activities
-    tLearn = 0; %param for when to update J matrix
-    iLearn = 1; %Used to index Adata to subtract from model predicted rates in err.
-    [epoch_LR,epoch_LU,epoch_LL,epoch_LD,epoch_RR,epoch_RU,epoch_RL,epoch_RD] = deal(0); %set epochs of external inputs to 0
-    
-    for tt = 2:length(t)-2 %time steps for each epoch. 
-        tLearn = tLearn + dt; %update for each time step
-        R(:, tt) = tanh(H); %nonlinear transformation of activities
-        if tLR(tt) == 1 %conditionals for training external input weights when stim is on
-            epoch_LR = 1;
-        end
-        if tLU(tt) == 1
-            epoch_LU = 1;
-        end
-        if tLL(tt) == 1
-            epoch_LL = 1;
-        end
-        if tLD(tt) == 1
-            epoch_LD = 1;
-        end
-        if tRR(tt) == 1
-            epoch_RR = 1;
-        end
-        if tRU(tt) == 1
-            epoch_RU = 1;
-        end
-        if tRL(tt) == 1
-            epoch_RL = 1;
-        end
-        if tRD(tt) == 1
-            epoch_RD = 1;
-        end
+if learn == True:
+    PJ = P0 * np.identity(N + num_of_inputs) # I don't think this should be called "PJ" unless it's related to J, but I'm not sure if it is
 
-        JR = J*R(:, tt) + inputN(:, tt); %product of synaptic strength weight matrix and nonlinear activities + gaussian noise
-            
-        %external inputs at each time step - input on if time vectors
-        %are 1 at time tt.
-        inputLR(:, tt) = amp_rgc*tLR(tt).*wLR;
-        inputLU(:, tt) = amp_rgc*tLU(tt).*wLU;
-        inputLL(:, tt) = amp_rgc*tLL(tt).*wLL;
-        inputLD(:, tt) = amp_rgc*tLD(tt).*wLD;
-        inputRR(:, tt) = amp_rgc*tRR(tt).*wRR;
-        inputRU(:, tt) = amp_rgc*tRU(tt).*wRU;
-        inputRL(:, tt) = amp_rgc*tRL(tt).*wRL;
-        inputRD(:, tt) = amp_rgc*tRD(tt).*wRD;
+print('Data steps: ', len(data_times))
+print('Data dt: ', dtData)
+print('Model dt: ', dtModel)
+print('Number of real seconds: ', len(real_times))
+print('Number of data timesteps: ', len(data_times))
+print('Number of model timesteps: ', len(model_times))
 
-        %add external inputs onto JR at each time step tt
-            
-        JR = JR + inputLR(:, tt) + inputLU(:, tt) + inputLL(:, tt) + inputLD(:, tt) + inputRR(:, tt) + inputRU(:, tt) + inputRL(:, tt) + inputRD(:, tt);
+chi2 = np.zeros(nRunTot) # used to be (1, nRunTot), but I think this is simpler
+pVars = np.zeros(nRunTot) # used to be (1, nRunTot), but I think this is simpler
+
+for nRun in range(nRunTot): # Epoch number, out of number of epochs [THIS SHOULD MAYBE BE CALLED A RUN?]
+    H = znn_acts[0] # Initialize RNN activities with ZNN activities at first time point
+    R[:,0] = np.tanh(H) # Nonlinearly transformed activities
+    tLearn = 0 # param for when to update J matrix
+    iLearn = 1 # Used to index znn_acts to subtract from model predicted rates in err.
+    # [epoch_LR,epoch_LU,epoch_LL,epoch_LD,epoch_RR,epoch_RU,epoch_RL,epoch_RD] = deal(0); # set epochs of external inputs to 0
+    input_epochs = np.zeros(num_of_inputs) # this replaced the line above
+
+    for tt in range(1, len(model_times)): # time steps for each epoch; used to be for tt = 2:len(t)-2
+        tLearn = tLearn + dtModel # update for each time step THIS IS JUST USED TO COUNT WHETHER THE TIMESTEP IS ONE WHERE AN ERROR CAN BE COMPUTED OR NOT
+        R[:, tt] = np.tanh(H) # nonlinear transformation of activities
+
+        for i in range(num_of_inputs):
+            if stim_course.iloc[i,tt] == 1: # conditionals for training external input weights when stim is on
+                input_epochs[i] == 1
         
-        H = H + dtModel*(-H + JR )/tau; %model prediction of calcium activities at each time step for every dt.
+        # generate external inputs at each time step - input on if time vectors are 1 at time tt
+        inputs = np.empty((num_of_inputs, N, len(model_times)))
+        for i in range(inputs.shape[0]):
+            for j in range(N):
+                print('*** TESTING ***')
+                print('i ', i)
+                print('j ', j)
+                print('tt ', tt)
+                print('amp_rgc ', amp_rgc)
+                print('stim_course.iloc[i,tt] ', stim_course.iloc[i,tt])
+                print('W_input[i] ', W_input[i])
+                print(inputs.shape)
+                print(W_input.shape)
+                inputs[i,j,tt] = amp_rgc * stim_course.iloc[i,tt] * W_input[i]
+
+        # Update RNN unit activities: multiply activities by weights, add noise, add inputs *** I DON'T THINK THESE SHOULD BE CALLED "JR"... THEY SHOULD JUST BE CALLED "R" ***
+        JR = J @ R[:,tt]
+        JR = JR + WN_input[:,tt]
+        JR = JR + inputs[:,:,tt] # Is this adding these in the right way?
+
+        H = H + (dtModel * (-H + JR)) / tau; # model prediction of calcium activities at each time step
         
-        if (tLearn >= dtData) %model updates weights if tLearn exceeds dtData. Since dtData = 2*dt, this happens every other time step.
-            tLearn = 0; 
-            err = JR - Adata(:, iLearn+1); %As in Andalman. Adata has entries every 0.5 s and JR at every 0.25 s.
-            meanerr2 = mean(err.^2); %what is displayed as chi2 when training. Use it to assess model convergence.
-            chi2(nRun) = chi2(nRun) + meanerr2; %nRun is epoch number so accumulates meanerr2 every update to weight matrix. Want this to decreaase over training
-            lastchi2s(iLearn) = meanerr2; %The last meanerr2 for the last weight update during an epoch.
-            if ((learn) && (nRun <= nRunTot - nFree) && (nRun > nFreePre)) %nFree and nFreePre are weight freezing parameters. learn is set to 1 at start of code.
-                AR = [R(:, tt); epoch_LR; epoch_LU; epoch_LL; epoch_LD; epoch_RR; epoch_RU; epoch_RL; epoch_RD]; %augmented Dyn variable. Each trainable external input is added here.
-                %For the next 4 lines you are computing an estimate of the 
-                %inverse cross correlation matrix of network rates that
-                %are used to scale the extent of the weight update.
-                %Further details in Sussillo & Abbott (2009). 
-                k = PJ*AR; 
-                rPr = AR'*k;
-                c = 1.0/(1.0 + rPr);
-                PJ = PJ - c*(k*k');
-                %Updating external input weights if they are on
-                if epoch_LR
-                    wLR = wLR - c*err*k(end-7);
-                end
-                if epoch_LU
-                    wLU = wLU - c*err*k(end-6);
-                end
-                if epoch_LL
-                    wLL = wLL - c*err*k(end-5);
-                end
-                if epoch_LD
-                    wLD = wLD - c*err*k(end-4);
-                end
-                if epoch_RR
-                    wRR = wRR - c*err*k(end-3);
-                end
-                if epoch_RU
-                    wRU = wRU - c*err*k(end-2);
-                end
-                if epoch_RL
-                    wRL = wRL - c*err*k(end-1);
-                end
-                if epoch_RD
-                    wRD = wRD - c*err*k(end);
-                end
-                
-                J = J - c*err*k(1:N)'; %update J by err and proportional to inverse cross correlation network rates
-                
-            end
-            iLearn = iLearn + 1; %Set index of Adata to time of next frame
-            [epoch_LR,epoch_LU,epoch_LL,epoch_LD,epoch_RR,epoch_RU,epoch_RL,epoch_RD]=deal(0); %Set epochs of external inputs to 0
-        end
-    end
+        if tLearn >= dtData: # model updates weights if tLearn exceeds dtData. Since dtData = 2*dt, this happens every other time step.
+            tLearn = 0
+            err = JR - znn_acts[:, iLearn+1] # As in Andalman. znn_acts has entries every 0.5 s and JR at every 0.25 s.
+            meanerr2 = np.mean(np.power(err, 2)) # what is displayed as chi2 when training. Use it to assess model convergence.
+            chi2[nRun] = chi2[nRun] + meanerr2 # nRun is epoch number so accumulates meanerr2 every update to weight matrix. Want this to decreaase over training
+            lastchi2s[iLearn] = meanerr2 # The last meanerr2 for the last weight update during an epoch.
+            if (learn == True) and (nRun <= nRunTot - nFree) and (nRun > nFreePre): # nFree and nFreePre are weight freezing parameters. learn is set to 1 at start of code.
+                # augmented Dyn variable. Each trainable external input is added here.
+                AR = R[:,tt]            
+                for i in range(num_of_inputs):
+                    AR = np.stack(AR, input_epochs[i])
 
-    %Summary of model fit - pVar means percentage of variance explained
-    rModelSample = R(:, iModelSample);
-    pVar = 1 - (norm(Adata - rModelSample, 'fro')/(sqrt(N*length(tData))*stdData)).^2;
-    pVars(nRun) = pVar;
-    fprintf('trial=%d pVar=%f chi2=%f\n', nRun, pVar, chi2(nRun));
-end
+                # compute estimate of inverse cross correlation matrix of network activities, to scale weight update. See Sussillo & Abbott (2009)
+                k = PJ @ AR  
+                rPr = np.transpose(AR) @ k
+                c = 1.0 / (1.0 + rPr)
+                PJ = PJ - c @ (k @ np.transpose(k))
 
-varData = var(reshape(Adata, N*length(tData_ind), 1));
-    chi2 = chi2/(sqrt(N*length(tData_ind))*varData);
-    lastchi2s = lastchi2s/(sqrt(N*length(tData_ind))*varData);
+                # Updating external input weights if they are on
+                for i in range(num_of_inputs):
+                    if input_epochs[i] == 1:
+                        W_input[:,i] = W_input[:,i] - c @ err @ k[i-num_of_inputs] # why is k indexed in this way?
+                
+                J = J - c @ err @ np.transpose(k[:N]) # update J by err and proportional to inverse cross correlation network rates
+                
+            iLearn = iLearn + 1 # Set index of Adata to time of next frame
+            input_epochs = np.zeros(len(input_epochs)) # Set epochs of external inputs to 0
+
+    # Summary of model fit - pVar means percentage of variance explained
+    rModelSample = R[:, iModelSample]    
+    pVar = 1 - np.power((np.linalg.norm(znn_acts - rModelSample) / (np.sqrt(N * len(data_times)) * stdevData)), 2)
+    pVars[nRun] = pVar
+    print('Run: {} \n pVar: {} \n chi2: {}'.format(nRun, pVar, chi2[nRun]))
+
+varData = np.var(np.reshape(znn_acts, N * len(data_times)), 1)   # transliterated this directly from the old matlab code. not sure if the dims are all right
+chi2 = chi2 / (np.sqrt(N * len(data_times)) * varData)
+lastchi2s = lastchi2s / (np.sqrt(N * len(data_times)) * varData)
     
-    %Variables you are saving from the model. You can load these variables from the
-    %output in matlab and save them as a different file type.
-    save(outputname, 'R', 'N', 't', 'chi2', 'Adata', 'tData', 'tData_ind', 'nRunTot', 'nFree', 'nFreePre', 'data_start_time', 'data_end_time', 'inputN', 'inputLR', 'inputLU', 'inputLL', 'inputLD', 'inputRR', 'inputRU', 'inputRL', 'inputRD', 'J', 'pVars', 'lastchi2s', 'simfilename', 'wLR', 'wLU', 'wLL', 'wLD', 'wRR', 'wRU', 'wRL', 'wRD', '-v7.3');
-   
-end
+'''
+Save the following data:
+    R, N, t, chi2, znn_acts(?), tData, tData_ind, nRunTot, nFree, nFreePre, data_start_time, data_end_time, inputN, inputs, J, pVars, lastchi2s, W_input
+'''
 
 ## Compute Statistics
 
--- python/sandbox.py
+++ python/sandbox.py
@@ -11,9 +11,27 @@ import numpy as np
 dtData = 0.5
 dtModel = 0.25
 
+tData = np.arange(0, 1000, 0.5)
+print(tData[0])
+print(tData[-1])
+print(len(tData))
+
+x = np.array([1, 2, 3])
+y = np.array([4, 5, 6])
+z = np.stack((x, y))
+print(z)
+
 znn_acts = pd.read_csv('znn_acts.csv', header=None) # create pandas dataframe of recorded znn calcium activities
+print(len(znn_acts[0]))
+print(len(znn_acts[1]))
+
+R = np.zeros((321, 1000)) # RNN unit activities *** AM I RIGHT IN USING MODEL TIMES HERE? ***
+print('len(R[0]) ', len(R[:,0]))
+
+stim_course = pd.read_csv('stimcourse.csv', header=None)
+print(stim_course.shape)
 print('Number of ZNN neurons: ', znn_acts.shape[0])
-'''
+
 data_start_time, real_start_time, model_start_time = np.zeros(3)   
 data_end_time = znn_acts.shape[1] # number of timepoints in data (number of activity measurements for each neuron)
 real_end_time = data_end_time * dtData  # number of seconds in data (how long were the data collected in real time in seconds)
@@ -27,23 +45,18 @@ real_times = np.arange(real_start_time, real_end_time, 1)
 data_times = np.arange(real_start_time, real_end_time, dtData)
 model_times = np.arange(real_start_time, real_end_time, dtModel)
 
-print('Real timeseries: ', real_times[0:20])
+print('Real timeseries: ', real_times[0:20], '...')
 print('Length of real timeseries: ', len(real_times))
-print('Data timeseries: ', data_times[0:20])
+print('Data timeseries: ', data_times[0:20], '...')
 print('Length of data timeseries: ', len(data_times))
-print('Model timeseries: ', model_times[0:20])
+print('Model timeseries: ', model_times[0:20], '...')
 print('Length of model timeseries: ', len(model_times))
 
 print(real_times[-1], data_times[-1], model_times[-1])
 
 lastchi2s = np.zeros(len(real_times))
 print(lastchi2s.shape)
-'''
 
-#stdData = std(reshape(znn_acts, N*data_end_time, 1))
-
-J0 = 1.25 * np.random.randn(350, 350) / np.sqrt(350)
-print(J0)
 
 ''' # removed this from main code. can just add nFreePre as an argument to fit(), default value 0, I think.
 try: 
@@ -51,3 +64,7 @@ try:
 except NameError:
     nFreePre = 0    # I still need to add a way for nFreePre to possibly be an input (and figure out what it does!)
 '''
+
+tData_ind = (1):(2000)
+data_start_0ndx = 0;
+data_end_0ndx = 1999
